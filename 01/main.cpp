d6ac693c3771afd4d9648c02537eb9c2d41bf86f
l <l>
2020-12-02 21:51:35 +0000

    - reformatting

--- 01/main.cpp
+++ 01/main.cpp
@@ -6,14 +6,14 @@
 std::vector<int> read_from_file(std::string filename)
 {
 	std::fstream myfile(filename, std::ios_base::in);
-    
-    std::vector<int> numbers{};
-    int a;
-    while (myfile >> a)
-    {
-        numbers.push_back(a);
-    }
-    return numbers;
+
+	std::vector<int> numbers{};
+	int a;
+	while (myfile >> a)
+	{
+		numbers.push_back(a);
+	}
+	return numbers;
 }
 
 std::pair<std::size_t, std::size_t> find_sum_indices(std::vector<int> numbers, int target_sum)
@@ -42,12 +42,12 @@ std::pair<std::size_t, std::size_t> find_sum_indices(std::vector<int> numbers, i
 
 int main(int argc, char * argv[])
 {
-    std::vector<int> numbers = read_from_file("./input_data.csv");
-    std::sort(numbers.begin(), numbers.end());
-    std::pair<std::size_t, std::size_t> sum_indices = find_sum_indices(numbers, 2020);
+	std::vector<int> numbers = read_from_file("./input_data.csv");
+	std::sort(numbers.begin(), numbers.end());
+	std::pair<std::size_t, std::size_t> sum_indices = find_sum_indices(numbers, 2020);
 
-   	std::cout << "Indices are " << sum_indices.first << " " << sum_indices.second << std::endl;
-   	std::cout << "Product is " << numbers[sum_indices.first] * numbers[sum_indices.second] << std::endl;
+	std::cout << "Indices are " << sum_indices.first << " " << sum_indices.second << std::endl;
+	std::cout << "Product is " << numbers[sum_indices.first] * numbers[sum_indices.second] << std::endl;
 
-    return 0;
+	return 0;
 }
